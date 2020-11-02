7c9f339dab4e6f08ec1714faf39e882edae05cc9
Willian <WN@WN03>
2020-11-02 09:20:09 -0300

    Criado comandos de Anexo

--- Manager.Domain.Core/Comandos/Tickets/ExcluirAnexo.cs
+++ Manager.Domain.Core/Comandos/Tickets/ExcluirAnexo.cs
@@ -5,5 +5,6 @@ namespace Manager.Domain.Core.Comandos.Tickets
     public class ExcluirAnexo : IRequest<Response>
     {
         public int IdAnexo { get; set; }
+        public int TicketId { get; set; }
     }
 }
---
+++ Manager.Domain.Core/Handlers/AnexoHandler.cs
@@ -0,0 +1,71 @@
+﻿using Flunt.Notifications;
+using Flunt.Validations;
+using Manager.Domain.Core.Comandos;
+using Manager.Domain.Core.Comandos.Tickets;
+using Manager.Domain.Entidades;
+using Manager.Domain.Interfaces.Repositorios;
+using MediatR;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Manager.Domain.Core.Handlers
+{
+    public class AnexoHandler : Notifiable, IRequestHandler<AdicionarAnexo, Response>, IRequestHandler<ExcluirAnexo, Response>
+    {
+        private readonly IRepositorioTicket _repositorioTicket;
+
+        public AnexoHandler(IRepositorioTicket repositorioTicket)
+        {
+            _repositorioTicket = repositorioTicket;
+        }
+
+        public async Task<Response> Handle(AdicionarAnexo request, CancellationToken cancellationToken)
+        {
+            if (request == null)
+                return new Response(false, "Informe os dados do anexo", request);
+
+            Ticket ticket = _repositorioTicket.CarregarObjetoPeloID(request.TicketId);
+
+            if (ticket == null)
+                return new Response(false, "Ticket não encontrado", null);
+
+            Anexo anexo = new Anexo(request.Descricao, request.URL, ticket);
+            ticket.AdicionarAnexo(anexo);
+
+            if (ticket.Invalid)
+                return new Response(false, "Ticket inválido", ticket.Notifications);
+
+            _repositorioTicket.Editar(ticket);
+
+            var result = new Response(true, "Anexo adicionado com sucesso!", null);
+            return await Task.FromResult(result);
+
+        }
+
+        public async Task<Response> Handle(ExcluirAnexo request, CancellationToken cancellationToken)
+        {
+            if (request == null)
+                return new Response(false, "Informe o anexo que deseja excluír", request);
+
+            Ticket ticket = _repositorioTicket.CarregarObjetoPeloID(request.TicketId);
+            Anexo anexo = ticket.Anexos.FirstOrDefault(a => a.Id == request.IdAnexo);
+
+            AddNotifications(new Contract()
+                .Requires()
+                .IsNotNull(ticket,"Ticket","Ticket não encontrado")
+                .IsNotNull(anexo,"Anexo","Anexo não encontrado")
+            );
+
+            ticket.ExcluirAnexo(anexo);
+
+            if (ticket.Invalid)
+                return new Response(false, "Ticket inválido", ticket.Notifications);
+
+            _repositorioTicket.Editar(ticket);
+
+            var result = new Response(true, "Anexo excluído com sucesso!", null);
+            return await Task.FromResult(result);
+        }
+    }
+}
